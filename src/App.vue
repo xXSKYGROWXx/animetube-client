1abc7002312368bff8da53d25b3d2193a7a0c09f
lai2-01 <lai2-01@itismeucci.com>
2023-04-17 12:37:28 +0200

    hommeStartedde

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 //localhost:5173
 import Header from './components/Header.vue'
+import Home from './views/Home.vue';
 </script>
 
 <template>
 <Header></Header>
+<Home></Home>
 </template>
 
 <style scoped>
--- src/components/ProfileTooltip.vue
+++ src/components/ProfileTooltip.vue
@@ -14,5 +14,7 @@
     width: 50px;
     height: 50px;
     border-radius: 50%;
+    margin-left: 25px;
+    margin-right: 5px;
 }
 </style>
